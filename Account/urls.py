5a39d2891aabef7558dd0fcd30aea5766e00d481
KaceTH <hh789123456@gmail.com>
2022-08-25 16:05:20 +0900

    first commit

---
+++ Account/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/', views.login),
+    path('user/', views.signUp),
+    path('user/<str:username>/', views.user_setting),
+    path('email_pass/<str:username>/', views.email_verification),
+    path('find_pw/', views.find_pw)
+]
